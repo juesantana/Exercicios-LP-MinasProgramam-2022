471784e407a5a588eec8b3d9fe249693c6f96ef7
juliana-santana <juesantana@gmail.com>
2022-06-13 10:31:50 -0300

    exercicio_1-Juliana_Santana

---
+++ exercicio_1.js
@@ -0,0 +1,11 @@
+
+const nome = 'Juliana';
+const idade = 22;
+const altura = 1.55;
+const verdadeiro = true;
+const falso = false;
+
+
+
+console.log(` Meu nome é  ${nome}, tenho ${idade} e ${altura} de altura`);
+console.log (`Sou estudante? Resposta: ${true}`)
