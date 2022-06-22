b80cc68c74765c58cfd9b75fbd1913c4d0ff33a6
juliana-santana <juesantana@gmail.com>
2022-06-22 15:30:42 -0300

    exercicio_2-Juliana_Santana

---
+++ exercicio_2.js
@@ -0,0 +1,6 @@
+let distancia = 40;
+console.log (`A distacia percorrida informada foi de ${distancia}m`);
+let tempo = 2;
+console.log (`O tempo percorrido infromada foi de ${tempo}s`);
+let velocidadeMedia = distancia/tempo;
+console.log (`A velocidade média é: ${velocidadeMedia} m/s`)
